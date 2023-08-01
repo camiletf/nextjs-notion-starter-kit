a5866b0eaa04fc75def7d6ae48558036f612786a
Dehao Qiu <90037477+camiletf@users.noreply.github.com>
2023-08-01 18:28:49 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '20fcdb46e94d4e5c9dfd1226c4dfc43f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '春风若有怜花意',
+  domain: 'https://www.notion.so/Personal-Home-20fcdb46e94d4e5c9dfd1226c4dfc43f',
+  author: 'Qiu DeHao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'https://github.com/camiletf?tab=overview&from=2023-07-01&to=2023-07-31',
   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
